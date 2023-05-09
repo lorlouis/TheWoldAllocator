6f6702975b227a9b35be01d03f692cee8d6f9bfc
lorlouis <louis.sven@gmail.com>
2023-05-09 00:57:49 -0400

    got the main part working how I wanted it but I can't differenciate between allocations made by the allocator and thus have a counter to decrease and allocations made my the system that do not have those

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,28 @@
 use std::alloc::{GlobalAlloc, System, Layout};
 
 use std::mem;
+use std::ptr;
 use lazy_static::lazy_static;
-use std::sync::atomic::{AtomicU64, Ordering};
 use std::backtrace::Backtrace;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::cell::Cell;
 use std::time::{SystemTime, UNIX_EPOCH};
+use nix::unistd;
+use std::sync::Mutex;
+use std::io::Write;
 
-use dashmap::DashMap;
-use std::collections::hash_map::RandomState;
+use hashbrown::{HashMap, hash_map::DefaultHashBuilder};
+
 
 lazy_static! {
-    static ref ALLOC_STATE: DashMap<String, Box<AtomicU64, System>, RandomState> = DashMap::default();
+    static ref ALLOC_STATE: Mutex<HashMap<Vec<u8, System>, Box<AtomicU64, System>, DefaultHashBuilder, System>> = Mutex::new(HashMap::new_in(System));
 }
 
 pub struct TheWorld;
 
 // stolen from dhat-rs
 struct IgnoreAllocs {
-    was_already_ignoreing_allocs: bool,
+    was_already_ignoring_allocs: bool,
 }
 
 thread_local!{
@@ -37,14 +41,14 @@ thread_local!{
 impl IgnoreAllocs {
     fn new() -> Self {
         Self {
-            was_already_ignoreing_allocs: IGNORE_ALLOCS.with(|b| b.replace(true)),
+            was_already_ignoring_allocs: IGNORE_ALLOCS.with(|b| b.replace(true)),
         }
     }
 }
 
 impl std::ops::Drop for IgnoreAllocs {
     fn drop(&mut self) {
-        if !self.was_already_ignoreing_allocs {
+        if !self.was_already_ignoring_allocs {
             IGNORE_ALLOCS.with(|b| b.set(false))
         }
     }
@@ -52,62 +56,64 @@ impl std::ops::Drop for IgnoreAllocs {
 // end of the stolen code
 
 unsafe impl GlobalAlloc for TheWorld {
+
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let ignore_allocs = IgnoreAllocs::new();
-        // speed up our own allocations
-        /*
-        if ignore_allocs.was_already_ignoreing_allocs {
-            return System.alloc(layout)
-        }
-        */
-        let rand = RNG.with(|rng| rng.u32(0..100));
 
-        let atomic_ptr = if rand <= 3 && !ignore_allocs.was_already_ignoreing_allocs {
-            let bt = Backtrace::capture().to_string();
-            let atomic = ALLOC_STATE.entry(bt)
-                .or_insert_with(|| Box::new_in(AtomicU64::new(0), System));
-            atomic.fetch_add(1, Ordering::Relaxed);
-            atomic.as_ref().as_ptr()
+        let alloc_surplus = layout.align().max(mem::size_of::<*const usize>());
+        let layout = Layout::from_size_align_unchecked(
+            layout.size() + alloc_surplus,
+            layout.align().max(mem::align_of::<*const u64>()),
+        );
+
+        let alloc = System.alloc(layout);
+        if ignore_allocs.was_already_ignoring_allocs {
+            alloc.write_bytes(0, mem::size_of::<*const u64>());
         }
         else {
-            0 as *mut u64
-        };
+            let rng = RNG.with(|rng| rng.i32(0..100));
 
-        let alloc_surplus = mem::size_of::<*mut u64>().max(layout.align());
-        let alloc_size = layout.size() + alloc_surplus;
-        let alloc_align = layout.align().max(mem::align_of::<*mut u64>());
+            let atomic_ptr = if rng <= 3 {
+                let mut buffer = Vec::new_in(System);
+                write!(&mut buffer, "{}", Backtrace::capture().to_string()).unwrap();
+                let ptr = ALLOC_STATE.lock().unwrap().entry(buffer)
+                    .or_insert_with(|| Box::new_in(AtomicU64::new(0), System)).as_ptr();
 
+                let counter = AtomicU64::from_ptr(ptr);
+                counter.fetch_add(1, Ordering::Relaxed);
 
-        let new_layout = Layout::from_size_align(alloc_size, alloc_align).unwrap();
+                let mut buffer = Vec::new_in(System);
+                writeln!(&mut buffer, "alloc: {:x}, ptr: {:x}\n", ptr as usize, alloc.add(alloc_surplus) as usize);
+                unistd::write(2, &buffer);
 
-        let system_ptr = System.alloc(new_layout);
-        let final_ptr = system_ptr.add(alloc_surplus);
-        let ptr_ptr = final_ptr.sub(mem::size_of::<*mut u64>());
-        *(ptr_ptr as *mut usize) = atomic_ptr as usize;
+                ptr
+            } else {
+                ptr::null_mut()
+            };
 
-        println!("sys: {:x} alloc: {:x}: {} {}", system_ptr as usize, final_ptr as usize, alloc_size, alloc_align);
+            ptr::write(alloc as *mut usize, atomic_ptr as usize);
+        }
 
-        final_ptr
+        alloc.add(alloc_surplus)
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        let ignore_allocs = IgnoreAllocs::new();
-        if ignore_allocs.was_already_ignoreing_allocs {
-            System.dealloc(ptr, layout);
-            return
-        }
-        let alloc_surplus = mem::size_of::<*mut u64>().max(layout.align());
-        let alloc_size = layout.size() + alloc_surplus;
-        let alloc_align = layout.align().max(mem::align_of::<*mut u64>());
+        let alloc_surplus = layout.align().max(mem::size_of::<*const ()>());
+        let layout = Layout::from_size_align_unchecked(
+            layout.size() + alloc_surplus,
+            layout.align().max(mem::align_of::<*const ()>()),
+        );
 
-        let ptr_ptr = ptr.sub(mem::size_of::<*mut u64>()) as *mut u64;
-        if !ptr_ptr.is_null() {
-            let atomic = AtomicU64::from_ptr(ptr_ptr);
-            atomic.fetch_sub(1, Ordering::AcqRel);
+        let atomic_ptr: *mut u64 = ptr::read(ptr as *const *mut u64);
+
+        if !atomic_ptr.is_null() {
+            let counter = AtomicU64::from_ptr(atomic_ptr);
+            let mut buffer = Vec::new_in(System);
+            writeln!(&mut buffer, "free: {:x}, ptr: {:x}\n", atomic_ptr as usize, ptr as usize);
+            unistd::write(2, &buffer);
+            counter.fetch_sub(1, Ordering::Relaxed);
         }
 
-        let new_layout = Layout::from_size_align(alloc_size, alloc_align).unwrap();
-        println!("arg: {:x} free: {:x}: {} {}", ptr as usize, ptr.sub(alloc_surplus) as usize, alloc_size, alloc_align);
-        System.dealloc(ptr.sub(alloc_surplus), new_layout)
+        System.dealloc(ptr.sub(alloc_surplus), layout);
     }
 }
--- tests/basic_alloc.rs
+++ tests/basic_alloc.rs
@@ -3,8 +3,7 @@ use the_world_allocator::TheWorld;
 #[global_allocator]
 static ALLOCATOR: TheWorld = TheWorld;
 
-#[test]
 fn main() {
-    let s: String = "hello".to_string();
-    assert_eq!(0, 0)
+    //let s: String = "hello".to_string();
+    //assert_eq!(0, 0)
 }
